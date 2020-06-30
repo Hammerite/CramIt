e95594c429de45185dc7b4b0c93f3be2b55287ce
Philip Eve <hammerite@orderofthehammer.com>
2020-06-30 23:02:08 +0100

    Add StandardRecipeGroupItemFilterer class, to allow filtering of items when there are multiple possible recipes

---
+++ Core/StandardRecipeGroupItemFilterer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CramIt.Core
+{
+    public class StandardRecipeGroupItemFilterer
+    {
+        public StandardRecipeGroupItemFilterer(IEnumerable<StandardRecipe> targetRecipes, IReadOnlyList<Item> alreadyChosenInputs)
+        {
+            if (targetRecipes is null)
+            {
+                throw new ArgumentNullException(nameof(targetRecipes));
+            }
+
+            _itemFilterers = targetRecipes.Select(r => new StandardRecipeItemFilterer(r, alreadyChosenInputs)).ToList();
+        }
+
+        private IReadOnlyList<StandardRecipeItemFilterer> _itemFilterers;
+
+        public bool CanCompleteAnyRecipeUsingItem(Item item)
+            => _itemFilterers.Any(f => f.CanCompleteRecipeUsingItem(item));
+    }
+}
