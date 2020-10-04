c623f31f4e367100ca1516f116b055057eb76430
Philip Eve <hammerite@orderofthehammer.com>
2020-10-04 22:07:49 +0100

    Add License info, and copyright statements to each file authored by me

--- Core/Items.cs
+++ Core/Items.cs
@@ -1,4 +1,18 @@
-﻿using System;
+﻿// Copyright 2020 Philip Eve
+//
+// This file is part of CRAM IT!.
+//
+// CRAM IT! is free software: you can redistribute it and/or modify it under the terms of the
+// GNU Affero General Public License, version 3, as published by the Free Software Foundation.
+//
+// CRAM IT! is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+// without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+// See the GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License along with this
+// program. If not, see <https://www.gnu.org/licenses/>.
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using static CramIt.Core.Type;
